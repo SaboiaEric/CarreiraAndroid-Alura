b401e9fec46b541584a94cecb3bcc605fcd5ed66
Eric Saboia <saboia.eric@gmail.com>
2017-07-01 16:27:30 -0300

    Curso 1 - Aula 3 - Completo - Adicionado options menu e botão de add aluno

--- Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,14 +14,24 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button btnSalvar = (Button) findViewById(R.id.formulario_salvar);
-        //Classe anônima para click do botão
-        btnSalvar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Infla, ou seja, exibe o xml (na pasta menu arquivo menu_formulario) no menu recebido como parametro
+        getMenuInflater().inflate(R.menu.menu_formulario,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this,"Salvo com sucesso!", Toast.LENGTH_SHORT).show();
-            }
-        });
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
