befcd8d93a8b2867d35d251cb02f3e7b79147b00
Eric Saboia <saboia.eric@gmail.com>
2017-07-04 06:50:43 -0300

    Curso 1 - Aula 4 - Completo - Iniciada a persistência de dados.

--- Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -8,19 +8,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
 public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-
-        String [] alunos = {"Eric","Carol","Olaf","Janaina","Tester"};
-        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,alunos);
-        listaAlunos.setAdapter(adapter);
-
         Button btnNovoAluno = (Button) findViewById(R.id.novo_aluno);
         btnNovoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +28,20 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void carregaLista() {
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
     }
 }
