b58fa491d7b23d44247dbec7d50bc3e3700c00da
Eric Saboia <saboia.eric@gmail.com>
2017-07-05 21:18:43 -0300

    Curso 2 - Aula 1 - Permissões, SMS, Mapas e Navegação - Concluido

--- Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -1,6 +1,12 @@
 package br.com.alura.agenda;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Browser;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActionMenuView;
@@ -16,6 +22,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Manifest;
 
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.modelo.Aluno;
@@ -57,13 +64,57 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(final ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        //Região ligação
+        MenuItem itemLigar = menu.add("Ligação");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //Verifica se a tela já tem permissão para a "Ligação Telefonica"
+                //Se existir continua a ligação, se não, pergunta se pode
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, android.Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{android.Manifest.permission.CALL_PHONE},123);
+                }
+                else{
+                Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                intentLigar.setData(Uri.parse("tel:"+aluno.getTelefone()));
+                startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
+
+
+
+        //Região de envio de sms
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone())); //Padrão de envio de sms do android
+        itemSMS.setIntent(intentSMS);
+
+        //Região de acesso Google Maps
+        MenuItem itemMAPA = menu.add("Visualizar Mapa");
+        Intent intentMAPA = new Intent(Intent.ACTION_VIEW);
+        intentMAPA.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco())); //Protocolo de acesso ao google maps do android
+        itemMAPA.setIntent(intentMAPA);
+
+        MenuItem itemSite = menu.add("Visualizar Site");
+        //Cria uma intent que utiliza uma action view, ou seja, chamará uma ação para um local.
+        Intent itentSite = new Intent(Intent.ACTION_VIEW);
+        String site = aluno.getSite();
+        if(!site.startsWith("http://"))
+            site = "http://" + site;
+        itentSite.setData(Uri.parse(site));
+        itemSite.setIntent(itentSite);
+
         MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
-
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
                 dao.close();
